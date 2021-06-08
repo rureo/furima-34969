d3b9382f6145bedf1c84c9544c03183a4fb7e3ed
rureo <reonakatsuru030623@gmail.com>
2021-06-08 21:31:58 +0900

    途中経過

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+ 
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birthday, :name_first, :name_last, :name_reading_first, :name_reading_last]
+    )
+  end
+ end
