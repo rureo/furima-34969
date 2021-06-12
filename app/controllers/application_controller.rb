b0535374e2d6d25ae91a9a6046915b9a982be13f
rureo <reonakatsuru030623@gmail.com>
2021-06-12 15:31:21 +0900

    テストコード修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
- 
-  protected
- 
-  def configure_permitted_parameters
 
+  protected
+
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :email, :birthday, :name_first, :name_last, :name_reading_first, :name_reading_last]
     )
   end
- end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :password, presence: true,format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-         validates :name_last, presence: true,format:{with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-         validates :name_first, presence: true,format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-         validates :name_reading_last, presence: true,format:{with: /\A[ァ-ヶー－]+\z/ }
-         validates :name_reading_first, presence: true,format: {with: /\A[ァ-ヶー－]+\z/ }
-         validates :birthday, presence: true
-
-        
+  validates :nickname, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :name_last, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :name_first, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :name_reading_last, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :name_reading_first, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
 end
