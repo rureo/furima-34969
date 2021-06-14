8db0df5b1a46e3b2b938dd219ff44b503aff6eef
rureo <reonakatsuru030623@gmail.com>
2021-06-14 19:52:14 +0900

    画像機能の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :who_pay_shipping_charge
+  belongs_to :ship_where_from
+  belongs_to :delivery_day
+  has_one_attached :image
+  
+with_options presence: true do
+  validates :title
+  validates :description
+  validates :category
+  validates :status
+  validates :who_pay_shipping_charge
+  validates :ship_where_from
+  validates :delivery_day
+
+  end
+
+  with_options  umericality: { other_than: 1 } 　do
+    validates :category_id
+    validates :statusid
+    validates :who_pay_shipping_charge_id
+    validates :ship_where_from_id
+    validates :delivery_day_id
+
+  end
+
+
+
 end
--- db/migrate/20210613055851_create_items.rb
+++ db/migrate/20210613055851_create_items.rb
@@ -1,6 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string     :title        , null: false
+      t.text       :text         , null: false
+      t.integer    :category_id     , null: false
+      t.integer    :status_id     , null: false
+      t.integer    :who_pay_shipping_charge_id     , null: false
+      t.integer    :ship_where_from_id    , null: false
+      t.integer    :delivery_day_id    , null: false
 
       t.timestamps
     end
