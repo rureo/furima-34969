561bc79113683ed66c3b034b498e4fbcdfd814a6
rureo <reonakatsuru030623@gmail.com>
2021-06-25 18:11:22 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
 
 
@@ -19,8 +19,16 @@ class ItemsController < ApplicationController
   end
 
 
-  
+
   def edit
+
+  end
+
+
+  def destroy
+   
+   @item.destroy
+   redirect_to root_path
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "items#index"
-  resources :items, only: [:index, :new,:create,:edit, :show, :update]
+  resources :items do
 
+  end
 end
