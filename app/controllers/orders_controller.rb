31071f912b015128e8194bb2ca6adeb76da7c757
rureo <reonakatsuru030623@gmail.com>
2021-07-07 16:23:03 +0900

    商品購入機能　残りの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :index]
   before_action :user_to_index, only: [:index]
-
+  before_action :sold_to_index, only: [:edit, :update, :create, :index]
  
   def index
     
@@ -10,7 +11,7 @@ class OrdersController < ApplicationController
   
   def create
     @item= Item.find(params[:item_id])
-    
+
 @order_shippment = OrderShippment.new(order_params)
 if @order_shippment.valid?
   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
@@ -46,9 +47,19 @@ end
 
 def user_to_index
   @item= Item.find(params[:item_id])
+ 
   if current_user.id == @item.user_id
     redirect_to  root_path
   end
+end
+
+
+
+def sold_to_index
+  if @item.order.present?
+    redirect_to  root_path
+  end
+
 end
 
 end
