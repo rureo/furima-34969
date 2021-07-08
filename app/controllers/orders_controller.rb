b05eefc2a8cf22e4c4128bc2ac4f88e2442ce6f4
rureo <reonakatsuru030623@gmail.com>
2021-07-08 18:57:26 +0900

    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,7 +30,7 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
-  end
+ 　 end
 
   def set_item
     @item=Item.find(params[:item_id])
